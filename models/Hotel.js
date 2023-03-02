91ac038ada4b3309e81e3604a27abf44286c2c08
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2023-03-02 15:41:39 +0200

    Add hotel model 0.1

---
+++ models/Hotel.js
@@ -0,0 +1,21 @@
+const {Schema, model, Types} = require('mongoose');
+
+const hotelSchema = new Schema({
+    name:{type:String,require:true, unique:true},
+    city:{type:String,require:true},
+    imageUrl:{type:String,require:true},
+    rooms:{type:Number,require:true},
+    bookings: {type: [Types.ObjectId], ref: 'User', default: []},
+    owner: {type: Types.ObjectId, ref: 'User' ,require:true}
+})
+
+hotelSchema.index({name:1}, {
+    collation:{
+        locale: 'en',
+        strength: 2
+    }
+})
+
+const Hotel = model('Hotel',hotelSchema);
+
+module.exports = Hotel;

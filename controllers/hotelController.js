a14403741096e6bc7a31eaeb948fa9053f2f804e
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2023-03-03 15:31:24 +0200

    Hotel controller adjust 0.1

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,11 +1,34 @@
+const {create, getById} = require('../services/hotelService')
 const hotelController = require('express').Router();
 
-hotelController.get('/:id/details',(req,res)=>{
-    res.render('details');
+hotelController.get('/:id/details',async (req,res)=>{
+    const hotel = await getById(req.params.id);
+    res.render('details',{
+        hotel
+    });
 })
 
-hotelController.get('create',(req,res)=>{
+hotelController.get('/create',(req,res)=>{
     res.render('create')
+})
+
+hotelController.post('/create',async(req,res)=>{
+    const hotel = {
+       name:req.body.name,
+       city:req.body.city,
+       rooms:req.body.rooms,
+       imageUrl: req.body.imageUrl, 
+       owner:req.user._id
+       //Todo warning switch room and img
+   
+    }
+
+    try{
+        await create(hotel);
+        res.redirect('/')
+    }catch(err){
+
+    }
 })
 
 hotelController.get('/:id/edit',(req,res)=>{

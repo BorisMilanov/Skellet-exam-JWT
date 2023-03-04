71c734cfb26056cb7699d69d6f6d52b6de294a22
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2023-03-04 17:28:10 +0200

    Add profile logic

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,16 @@
+const {hasUser} = require('../middlewares/guards');
+const {getByUserBooking} = require('../services/hotelService');
+
+
 const profileController = require('express').Router();
 
-profileController.get('/',(req,res)=>{
-    res.render('profile')
+profileController.get('/',hasUser(),async(req,res)=>{
+    const bookings = await getByUserBooking(req.user._id);
+    console.log(bookings);
+    res.render('profile',{
+        user:Object.assign({bookings}, req.user)
+
+    })
 })
 
 module.exports = profileController;
--- services/hotelService.js
+++ services/hotelService.js
@@ -1,34 +1,52 @@
 const Hotel = require("../models/Hotel");
 
-async function getAll(){
+async function getAll() {
     return Hotel.find({}).lean()
 }
 
-async function getById(id){
+async function getById(id) {
     return Hotel.findById(id).lean()
 }
 
-async function create(hotel){
+async function create(hotel) {
     return await Hotel.create(hotel)
 }
 
-async function update(id, hotel){
+async function update(id, hotel) {
+    const existing = await Hotel.findById(id);
 
+    existing.name = hotel.name;
+    existing.city = hotel.city;
+    existing.rooms = hotel.rooms;
+    existing.imageUrl = hotel.imageUrl;
+
+    await existing.save();
 }
 
-async function deleteById(id){
-
+async function deleteById(id) {
+    await Hotel.findByIdAndRemove(id)
 }
+async function getByUserBooking(userId) {
+    return (await Hotel.find({ bookings: userId }).lean())
+}
+async function bookRoom(hotelId, userId) {
+    const hotel = await Hotel.findById(hotelId);
 
-async function bookRoom(hotelId,userId){
+    if (hotel.bookings.includes(userId)) {
+        throw new Error('Cannot book twice')
+    }
 
+    hotel.bookings.push(userId);
+    await hotel.save()
 }
 
 module.exports = {
     getAll,
     getById,
     create,
-    update, 
+    update,
     deleteById,
-    bookRoom
+    bookRoom,
+    getByUserBooking
+
 }
